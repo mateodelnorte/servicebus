e370f9ae125a741d7a6836a6dc963c2b423e4e74
Ryan VanMiddlesworth <ryan@vanmiddlesworth.org>
2017-01-26 12:27:43 -0500

    Added test cases for closed connections

---
+++ test/conn-channel-events.js
@@ -0,0 +1,52 @@
+var log = require('debug')('servicebus:test');
+var sinon = require('sinon');
+var util = require('util');
+
+describe('servicebus', function(){
+
+  describe('#close', function() {
+
+    var bus;
+
+    beforeEach(function(done) {
+      bus = require('../').bus({ prefetch: 5, url: process.env.RABBITMQ_URL });
+
+      if (!bus.initialized) {
+        bus.on('ready', done);
+      } else {
+        done();
+      }
+    });
+
+    it('should emit connection_close event', function (done){
+
+      bus.on('connection_close', function (event) {
+        done();
+      });
+
+      bus.listen('my.event.50', function (event) {
+        bus.close();
+      });
+
+      setTimeout(function () {
+        bus.send('my.event.50', { my: 'event' });
+      }, 10);
+    });
+
+    it('should emit channel_close event', function (done){
+
+      bus.once('channel_close', function (event) {
+        done();
+      });
+
+      bus.listen('my.event.51', function (event) {
+        bus.close();
+      });
+
+      setTimeout(function () {
+        bus.send('my.event.51', { my: 'event' });
+      }, 10);
+    });
+
+  });
+});
